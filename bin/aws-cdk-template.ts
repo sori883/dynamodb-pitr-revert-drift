42ca4baa14bb1c0fb0f3391c6861f8c417618263
sori883 <37400002+sori883@users.noreply.github.com>
2026-01-18 10:54:10 +0900

    ドリフト確認

--- bin/aws-cdk-template.ts
+++ bin/aws-cdk-template.ts
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
 
-import { AppStack } from '../lib/app-stack';
-import { DataStack } from '../lib/data-stack';
 import { InfraStack } from '../lib/infra-stack';
 import { parameter as p } from '../parameter';
 import { validateEnvName } from '../parameter/envname-type';
@@ -16,24 +14,8 @@ cdk.Tags.of(app).add('Project', parameter.project);
 cdk.Tags.of(app).add('Cost', parameter.cost);
 cdk.Tags.of(app).add('Owner', parameter.owner);
 
-const infra = new InfraStack(app, 'Infra', {
+new InfraStack(app, 'Infra', {
   stackName: `${parameter.prefix}-infra`,
   env: { account: parameter.dotEnv.ACCOUNT_ID, region: parameter.region },
   parameter,
-});
-
-new DataStack(app, 'Data', {
-  stackName: `${parameter.prefix}-data`,
-  env: { account: parameter.dotEnv.ACCOUNT_ID, region: parameter.region },
-  parameter,
-  vpc: infra.vpc,
-  sgRds: infra.sgRds,
-});
-
-new AppStack(app, 'App', {
-  stackName: `${parameter.prefix}-app`,
-  env: { account: parameter.dotEnv.ACCOUNT_ID, region: parameter.region },
-  parameter,
-  vpc: infra.vpc,
-  sgEc2: infra.sgEc2,
 });
--- lib/infra-stack.ts
+++ lib/infra-stack.ts
@@ -1,12 +1,10 @@
-import type * as ec2 from 'aws-cdk-lib/aws-ec2';
 import type { Construct } from 'constructs';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib/core';
 
 import type { ParameterType } from '../parameter';
-import { Vpc } from './constructs/network/vpc';
-import { VpcEndpoint } from './constructs/network/vpce';
-import { SgApp } from './constructs/security-group/sg-app';
-import { SgVpce } from './constructs/security-group/sg-vpce';
 
 interface StackProps extends cdk.StackProps {
   readonly parameter: ParameterType;
@@ -16,37 +14,29 @@ interface StackProps extends cdk.StackProps {
  * インフラ
  */
 export class InfraStack extends cdk.Stack {
-  public readonly vpc: ec2.IVpc;
-  public readonly sgEc2: ec2.ISecurityGroup;
-  public readonly sgRds: ec2.ISecurityGroup;
-
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
-    const { parameter } = props;
 
-    const vpc = new Vpc(this, 'VPC', {
-      publicNats: parameter.diffEnv.vpc.publicNats,
-      cidr: parameter.diffEnv.vpc.cidr,
-      maxAzs: parameter.diffEnv.vpc.maxAzs,
-      subnetConfigs: Object.values(parameter.diffEnv.vpc.subnets),
+    // DynamoDB Table
+    const table = new dynamodb.Table(this, 'Table', {
+      partitionKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      pointInTimeRecovery: true,
     });
-    this.vpc = vpc.vpc;
 
-    const sgVpce = new SgVpce(this, 'SgVpce', {
-      vpc: vpc.vpc,
-    });
-    const sgApp = new SgApp(this, 'SgApp', {
-      vpc: vpc.vpc,
-    });
-    this.sgEc2 = sgApp.sgEc2;
-    this.sgRds = sgApp.sgRds;
+    cdk.Tags.of(table).add('Project', 'DynamoDBPITRRevertDrift');
+    cdk.Tags.of(table).add('ManagedBy', 'CDK');
 
-    new VpcEndpoint(this, 'VpcEndpoint', {
-      vpc: vpc.vpc,
-      SgVpce: sgVpce.sgVpce,
-      vpceSubnets: vpc.vpc.selectSubnets({
-        subnetGroupName: parameter.diffEnv.vpc.subnets.Private.name,
-      }),
+    // S3 Bucket
+    new s3.Bucket(this, 'Bucket', {
+      versioned: true,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      autoDeleteObjects: true,
+      removalPolicy: RemovalPolicy.DESTROY,
     });
   }
 }
